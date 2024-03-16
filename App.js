768c54dddaff8d773eb35e754da5d03f1097ecbe
Mohammad Kashif Sulaiman <m.kqagraphix@gmail.com>
2024-03-16 12:22:16 +0500

    remove far car

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, Text, View, Modal, Button } from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
 import { Accelerometer } from 'expo-sensors';
 
 export default function App() {
@@ -22,20 +22,12 @@ export default function App() {
     return () => _unsubscribe();
   }, []);
 
-  // console.log('data', data.x * 200)
-  //left: +
-  //right: -
 
   return (
     <View style={styles.container}>
       <Image
         style={{ flex: 1 }}
         source={{ uri: 'https://i.pinimg.com/originals/23/df/85/23df8576ec3f71e173de2911a597fb9e.gif' }}
-      />
-
-      <Image
-        style={{ width: 60, height: 60, position: 'absolute', top: 270, right: (data.x) * 120 }}
-        source={{ uri: 'https://static.vecteezy.com/system/resources/thumbnails/017/173/213/small/car-isolated-on-white-back-view-png.png' }}
       />
 
       <Image
@@ -52,14 +44,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'gray',
-    // justifyContent: 'center',
-    // alignItems: 'center'
   },
 });
-
-
-/*
-1. Parent component me function banega jo state ko change karega
-2. Is function ko child me pass karenge
-3. Child me event per us function ko call kardenge
-*/
